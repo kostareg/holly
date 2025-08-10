4d13175226b040d3ad0ecdd54ab78be0a3164a86
kostareg <areg.kostanyan@gmail.com>
2025-08-09 23:42:15 -0400

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,147 @@
+{
+  description = "Hello world flake using uv2nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    uv2nix,
+    pyproject-nix,
+    pyproject-build-systems,
+    ...
+  }: let
+    inherit (nixpkgs) lib;
+
+    workspace = uv2nix.lib.workspace.loadWorkspace {workspaceRoot = ./.;};
+
+    overlay = workspace.mkPyprojectOverlay {
+      sourcePreference = "wheel";
+    };
+
+    pyprojectOverrides = _final: _prev: {
+    };
+
+    # todo: flake-utils and build for all platforms
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+    python = pkgs.python312;
+
+    pythonSet =
+      (pkgs.callPackage pyproject-nix.build.packages {
+        inherit python;
+      }).overrideScope
+      (
+        lib.composeManyExtensions [
+          pyproject-build-systems.overlays.default
+          overlay
+          pyprojectOverrides
+        ]
+      );
+  in {
+    packages.x86_64-linux.default = pythonSet.mkVirtualEnv "holly-env" workspace.deps.default;
+
+    apps.x86_64-linux = {
+      default = {
+        type = "app";
+        program = "${self.packages.x86_64-linux.default}/bin/holly";
+      };
+    };
+
+    devShells.x86_64-linux = {
+      impure = pkgs.mkShell {
+        packages = [
+          python
+          pkgs.uv
+
+          pkgs.bun
+        ];
+        env =
+          {
+            UV_PYTHON_DOWNLOADS = "never";
+            UV_PYTHON = python.interpreter;
+          }
+          // lib.optionalAttrs pkgs.stdenv.isLinux {
+            LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
+          };
+        shellHook = ''
+          unset PYTHONPATH
+        '';
+      };
+
+      # todo: set up some build system for the web interface too?
+      uv2nix = let
+        editableOverlay = workspace.mkEditablePyprojectOverlay {
+          root = "$REPO_ROOT";
+        };
+
+        editablePythonSet = pythonSet.overrideScope (
+          lib.composeManyExtensions [
+            editableOverlay
+
+            (final: prev: {
+              holly = prev.holly.overrideAttrs (old: {
+                src = lib.fileset.toSource {
+                  root = old.src;
+                  fileset = lib.fileset.unions [
+                    (old.src + "/pyproject.toml")
+                    (old.src + "/readme.md")
+                    (old.src + "/src/holly/__init__.py")
+                  ];
+                };
+
+                nativeBuildInputs =
+                  old.nativeBuildInputs
+                  ++ final.resolveBuildSystem {
+                    editables = [];
+                  };
+              });
+            })
+          ]
+        );
+
+        virtualenv = editablePythonSet.mkVirtualEnv "holly-dev-env" workspace.deps.all;
+      in
+        pkgs.mkShell {
+          packages = [
+            virtualenv
+            pkgs.uv
+          ];
+
+          env = {
+            UV_NO_SYNC = "1";
+            UV_PYTHON = python.interpreter;
+            UV_PYTHON_DOWNLOADS = "never";
+          };
+
+          shellHook = ''
+            # Undo dependency propagation by nixpkgs.
+            unset PYTHONPATH
+
+            # Get repository root using git. This is expanded at runtime by the editable `.pth` machinery.
+            export REPO_ROOT=$(git rev-parse --show-toplevel)
+          '';
+        };
+    };
+  };
+}
