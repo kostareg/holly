2f3854a816206c748acad1ba0b883080fbf5be84
kostareg <areg.kostanyan@gmail.com>
2025-08-10 21:04:44 -0400

    feat(web): basic charts

--- web/src/routes/live.tsx
+++ web/src/routes/live.tsx
@@ -1,7 +1,18 @@
-import { Button } from "@/components/ui/button";
+import { ChartContainer, type ChartConfig } from "@/components/ui/chart";
+import { Line, LineChart } from "recharts";
+
+const data = [{ name: 'Some content', uv: 400, pv: 2400, amt: 2400 }, { uv: 100 }, { uv: 200 }];
+
+const config = {} satisfies ChartConfig;
 
 function Live() {
-	return <Button>Hello, world!</Button>;
+  return <div className="grid grid-cols-4 gap-4">
+    <ChartContainer config={config} className="size-full">
+      <LineChart data={data}>
+        <Line dataKey="uv" />
+      </LineChart>
+    </ChartContainer>
+  </div>;
 }
 
 export default Live;
